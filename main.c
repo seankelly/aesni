60ad6d206abc1aecef270e4ec6e37e51deebc3a6
Sean Kelly <code@katron.org>
2014-07-09 20:50:30 -0500

    Allow specifying number of blocks to test

--- main.c
+++ main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "aes.h"
 
 int
 main(int argc, char **argv)
 {
+	long blocks;
+	char *nptr;
+
 	aes_init();
+
+	blocks = 10;
+	if (argc > 1)
+	{
+		long b = strtol(argv[1], &nptr, 10);
+		if (argv[1] != nptr)
+		{
+			blocks = b;
+		}
+	}
 
 	return 0;
 }
