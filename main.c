f4428107221ef234a2a1f25d6221920ace1894ce
Sean Kelly <code@katron.org>
2014-07-09 21:09:48 -0500

    Add framework to read random bytes and encrypt
    Bytes will be read from urandom for the key and input. Input will be
    copied and both inputs will be encrypted with the key. Outputs will be
    compared to check if they match.

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "aes.h"
+
+static void
+test_aes_encrypt(long blocks)
+{
+	long i;
+	int fd;
+	char key[32], openssl[16], my_aes[16];
+
+	fd = open("/dev/urandom", O_RDONLY|O_CLOEXEC);
+	if (fd < 0)
+	{
+		return;
+	}
+
+	for (i = 0; i < blocks; i++)
+	{
+	}
+
+	close(fd);
+}
 
 int
 main(int argc, char **argv)
@@ -19,6 +44,11 @@ main(int argc, char **argv)
 		{
 			blocks = b;
 		}
+	}
+
+	if (blocks > 0)
+	{
+		test_aes_encrypt(blocks);
 	}
 
 	return 0;
