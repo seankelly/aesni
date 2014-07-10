ed614ec29ae9c8b6e98d7b2d87c6c09002a6e99d
Sean Kelly <code@katron.org>
2014-07-09 21:48:34 -0500

    Encrypt
    Using OpenSSL for both right now.

--- aes.c
+++ aes.c
@@ -2,23 +2,24 @@
 
 #include "aes.h"
 
-static void aes_encrypt_block_openssl(const unsigned char *, unsigned char *, unsigned char *);
-static void aes_encrypt_block_aesni(const unsigned char *, unsigned char *, unsigned char *);
+static void aes_encrypt_block_openssl(const unsigned char *, unsigned char *, const AES_KEY *);
+static void aes_encrypt_block_aesni(const unsigned char *, unsigned char *, const AES_KEY *);
 
 void
 aes_init(void)
 {
+	aes_encrypt_block = aes_encrypt_block_aesni;
 	aes_encrypt_block = aes_encrypt_block_openssl;
 }
 
 static void
-aes_encrypt_block_openssl(const unsigned char *in, unsigned char *out, unsigned char *key)
+aes_encrypt_block_openssl(const unsigned char *in, unsigned char *out, const AES_KEY *key)
 {
-	printf("openssl\n");
+	AES_encrypt(in, out, key);
 }
 
 static void
-aes_encrypt_block_aesni(const unsigned char *in, unsigned char *out, unsigned char *key)
+aes_encrypt_block_aesni(const unsigned char *in, unsigned char *out, const AES_KEY *key)
 {
-	printf("aesni\n");
+	AES_encrypt(in, out, key);
 }
