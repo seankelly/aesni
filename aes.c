75bcf8f72273fed5010bfc8508e2f3e6955084f4
Sean Kelly <code@katron.org>
2014-07-12 08:32:06 -0500

    Use unaligned loads before using AES-NI
    I have no guarantee the data is aligned on 16-byte boundaries, so need
    to do unaligned loads. Otherwise segfaults!

--- aes.c
+++ aes.c
@@ -73,32 +73,75 @@ aes_encrypt_block_aesni(const uint8_t *in, uint8_t *out, const AES_KEY *key)
 
 	memcpy(aes_key, key->rd_key, sizeof(aes_key));
 
-	asm volatile ("movdqu %1, %0" : "=x" (aes_state) : "m" (in[0]));
-	asm volatile ("pxor   %1, %0" : "=x" (aes_state) : "m" (aes_key[0]));
-	asm volatile ("aesenc %1, %0" : "=x" (aes_state) : "m" (aes_key[16]));
-	asm volatile ("aesenc %1, %0" : "=x" (aes_state) : "m" (aes_key[32]));
-	asm volatile ("aesenc %1, %0" : "=x" (aes_state) : "m" (aes_key[48]));
-	asm volatile ("aesenc %1, %0" : "=x" (aes_state) : "m" (aes_key[64]));
-	asm volatile ("aesenc %1, %0" : "=x" (aes_state) : "m" (aes_key[80]));
-	asm volatile ("aesenc %1, %0" : "=x" (aes_state) : "m" (aes_key[96]));
-	asm volatile ("aesenc %1, %0" : "=x" (aes_state) : "m" (aes_key[112]));
-	asm volatile ("aesenc %1, %0" : "=x" (aes_state) : "m" (aes_key[128]));
-	asm volatile ("aesenc %1, %0" : "=x" (aes_state) : "m" (aes_key[144]));
+	/*
+	 * Load in block into xmm0. Blocks from round keys will be loaded into
+	 * xmm1-xmm7, rotating around as necessary. Intermediate state and
+	 * final result are in xmm0.
+	 * Loading into memory using unaligned loads because can't guarantee at
+	 * this moment that any of the input or output variables are aligned on
+	 * a 16-byte boundary.
+	 */
+	asm volatile (
+		"movdqu %0, %%xmm0\n\t"
+		"movdqu %1, %%xmm1\n\t"
+		"movdqu %2, %%xmm2\n\t"
+		"movdqu %3, %%xmm3\n\t"
+		"movdqu %4, %%xmm4\n\t"
+		"movdqu %5, %%xmm5\n\t"
+		"movdqu %6, %%xmm6\n\t"
+		"movdqu %7, %%xmm7\n\t"
+		"pxor   %%xmm1, %%xmm0\n\t"
+		"aesenc %%xmm2, %%xmm0\n\t"
+		"aesenc %%xmm3, %%xmm0\n\t"
+		"aesenc %%xmm4, %%xmm0\n\t"
+		"aesenc %%xmm5, %%xmm0\n\t"
+		"aesenc %%xmm6, %%xmm0\n\t"
+		"aesenc %%xmm7, %%xmm0\n\t"
+		"movdqu %8, %%xmm1\n\t"
+		"movdqu %9, %%xmm2\n\t"
+		"movdqu %10, %%xmm3\n\t"
+		"aesenc %%xmm1, %%xmm0\n\t"
+		"aesenc %%xmm2, %%xmm0\n\t"
+		"aesenc %%xmm3, %%xmm0\n\t"
+		:
+		: "m" (in[0]), "m" (aes_key[0]), "m" (aes_key[16]),
+		"m" (aes_key[32]), "m" (aes_key[48]), "m" (aes_key[64]),
+		"m" (aes_key[80]), "m" (aes_key[96]), "m" (aes_key[112]),
+		"m" (aes_key[128]), "m" (aes_key[144])
+	);
 	final_index = 160;
 
 	if (key->rounds > 10) {
-		asm volatile ("aesenc %1, %0" : "=x" (aes_state) : "m" (aes_key[160]));
-		asm volatile ("aesenc %1, %0" : "=x" (aes_state) : "m" (aes_key[176]));
 		final_index = 192;
+		asm volatile (
+			"movdqu %0, %%xmm4\n\t"
+			"movdqu %1, %%xmm5\n\t"
+			"aesenc %%xmm4, %%xmm0\n\t"
+			"aesenc %%xmm5, %%xmm0\n\t"
+			:
+			: "m" (aes_key[160]), "m" (aes_key[176])
+		);
 
 		if (key->rounds > 12) {
-			asm volatile ("aesenc %1, %0" : "=x" (aes_state) : "m" (aes_key[192]));
-			asm volatile ("aesenc %1, %0" : "=x" (aes_state) : "m" (aes_key[208]));
+			asm volatile (
+				"movdqu %0, %%xmm6\n\t"
+				"movdqu %1, %%xmm7\n\t"
+				"aesenc %%xmm6, %%xmm0\n\t"
+				"aesenc %%xmm7, %%xmm0\n\t"
+				:
+				: "m" (aes_key[192]), "m" (aes_key[208])
+			);
 			final_index = 224;
 		}
 	}
 
-	asm volatile ("aesenclast %1, %0" : "=x" (aes_state) : "m" (aes_key[final_index]));
+	asm volatile (
+		"movdqu %1, %%xmm1\n\t"
+		"aesenclast %%xmm1, %%xmm0\n\t"
+		"movdqu %%xmm0, %0\n\t"
+		: "=m" (aes_state)
+		: "m" (aes_key[final_index])
+	);
 
 	memcpy(out, &aes_state, sizeof(aes_state));
 
