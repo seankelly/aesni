f971ac6b8b13fcafbf0de802990e86af55ea13bd
Sean Kelly <code@katron.org>
2014-07-29 22:04:25 -0500

    Switch to using intrinsics

--- aes.c
+++ aes.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <wmmintrin.h>
 
 #include "aes.h"
 
@@ -61,83 +62,36 @@ aes_encrypt_block_openssl(const uint8_t *in, uint8_t *out, const AES_KEY *key)
 static void
 aes_encrypt_block_aesni(const uint8_t *in, uint8_t *out, const AES_KEY *key)
 {
-#if 0
-	AES_encrypt(in, out, key);
-#else
 	int final_index;
-	const uint32_t *aes_key;
+	const __m128i *aes_key;
+	__m128i aes_state;
 
-	aes_key = key->rd_key;
+	aes_key = (const __m128i *)key->rd_key;
 
-	/*
-	 * Load in block into xmm0. Blocks from round keys will be loaded into
-	 * xmm1-xmm7, rotating around as necessary. Intermediate state and
-	 * final result are in xmm0.
-	 * Loading into memory using unaligned loads because can't guarantee at
-	 * this moment that any of the input or output variables are aligned on
-	 * a 16-byte boundary.
-	 */
-	asm volatile (
-		"movdqu %0, %%xmm0\n\t"
-		"movdqu %1, %%xmm1\n\t"
-		"movdqu %2, %%xmm2\n\t"
-		"movdqu %3, %%xmm3\n\t"
-		"movdqu %4, %%xmm4\n\t"
-		"movdqu %5, %%xmm5\n\t"
-		"movdqu %6, %%xmm6\n\t"
-		"movdqu %7, %%xmm7\n\t"
-		"pxor   %%xmm1, %%xmm0\n\t"
-		"aesenc %%xmm2, %%xmm0\n\t"
-		"aesenc %%xmm3, %%xmm0\n\t"
-		"aesenc %%xmm4, %%xmm0\n\t"
-		"aesenc %%xmm5, %%xmm0\n\t"
-		"aesenc %%xmm6, %%xmm0\n\t"
-		"aesenc %%xmm7, %%xmm0\n\t"
-		"movdqu %8, %%xmm1\n\t"
-		"movdqu %9, %%xmm2\n\t"
-		"movdqu %10, %%xmm3\n\t"
-		"aesenc %%xmm1, %%xmm0\n\t"
-		"aesenc %%xmm2, %%xmm0\n\t"
-		"aesenc %%xmm3, %%xmm0"
-		:
-		: "m" (in[0]), "m" (aes_key[0]), "m" (aes_key[4]),
-		"m" (aes_key[8]), "m" (aes_key[12]), "m" (aes_key[16]),
-		"m" (aes_key[20]), "m" (aes_key[24]), "m" (aes_key[28]),
-		"m" (aes_key[32]), "m" (aes_key[36])
-	);
-	final_index = 40;
-
+	aes_state = _mm_loadu_si128((__m128i *)in);
+	aes_state = _mm_xor_si128(aes_state, aes_key[0]);
+	aes_state = _mm_aesenc_si128(aes_state, aes_key[1]);
+	aes_state = _mm_aesenc_si128(aes_state, aes_key[2]);
+	aes_state = _mm_aesenc_si128(aes_state, aes_key[3]);
+	aes_state = _mm_aesenc_si128(aes_state, aes_key[4]);
+	aes_state = _mm_aesenc_si128(aes_state, aes_key[5]);
+	aes_state = _mm_aesenc_si128(aes_state, aes_key[6]);
+	aes_state = _mm_aesenc_si128(aes_state, aes_key[7]);
+	aes_state = _mm_aesenc_si128(aes_state, aes_key[8]);
+	aes_state = _mm_aesenc_si128(aes_state, aes_key[9]);
+	final_index = 10;
 	if (key->rounds > 10) {
-		final_index = 48;
-		asm volatile (
-			"movdqu %0, %%xmm4\n\t"
-			"movdqu %1, %%xmm5\n\t"
-			"aesenc %%xmm4, %%xmm0\n\t"
-			"aesenc %%xmm5, %%xmm0"
-			:
-			: "m" (aes_key[40]), "m" (aes_key[44])
-		);
+		aes_state = _mm_aesenc_si128(aes_state, aes_key[10]);
+		aes_state = _mm_aesenc_si128(aes_state, aes_key[11]);
+		final_index = 12;
 
 		if (key->rounds > 12) {
-			asm volatile (
-				"movdqu %0, %%xmm6\n\t"
-				"movdqu %1, %%xmm7\n\t"
-				"aesenc %%xmm6, %%xmm0\n\t"
-				"aesenc %%xmm7, %%xmm0"
-				:
-				: "m" (aes_key[48]), "m" (aes_key[52])
-			);
-			final_index = 56;
+			aes_state = _mm_aesenc_si128(aes_state, aes_key[12]);
+			aes_state = _mm_aesenc_si128(aes_state, aes_key[13]);
+			final_index = 14;
 		}
 	}
 
-	asm volatile (
-		"movdqu %1, %%xmm1\n\t"
-		"aesenclast %%xmm1, %%xmm0\n\t"
-		"movdqu %%xmm0, %0"
-		: "=m" (*out)
-		: "m" (aes_key[final_index])
-	);
-
-#endif
+	aes_state = _mm_aesenclast_si128(aes_state, aes_key[final_index]);
+	_mm_storeu_si128((__m128i *)out, aes_state);
 }
