f432e9d7117c8c06184fb3272e8f350b19e5547e
Sean Kelly <code@katron.org>
2014-07-30 21:32:00 -0500

    Fix indenting

--- cpusupport.h
+++ cpusupport.h
@@ -5,11 +5,11 @@ int cpusupport_x86_aesni_detect(void);
 static inline int
 cpusupport_x86_aesni(void)
 {
-    if (cpusupport_x86_aesni_present)
-        return (1);
-    else if (cpusupport_x86_aesni_init)
-        return (0);
-    cpusupport_x86_aesni_present = cpusupport_x86_aesni_detect();
-    cpusupport_x86_aesni_init = 1;
-    return (cpusupport_x86_aesni_present);
+	if (cpusupport_x86_aesni_present)
+		return (1);
+	else if (cpusupport_x86_aesni_init)
+		return (0);
+	cpusupport_x86_aesni_present = cpusupport_x86_aesni_detect();
+	cpusupport_x86_aesni_init = 1;
+	return (cpusupport_x86_aesni_present);
 }
